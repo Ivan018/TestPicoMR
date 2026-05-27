7a171b46bc03cf53f1c6ba34fd1698a01aa21d88
fanhua_wd <fanhua@wondertek.com.cn>
2026-05-27 17:50:46 +0800

    feat: 采用pico原生sdk项目所需修改

--- Assets/Scripts/OpenXRSetting.cs
+++ Assets/Scripts/OpenXRSetting.cs
@@ -1,24 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.XR.OpenXR.Features.PICOSupport;
+using Unity.XR.PXR;
+
+//using Unity.XR.OpenXR.Features.PICOSupport;
 using UnityEngine;
 
 public class OpenXRSetting : MonoBehaviour
 {
     private void Awake()
     {
-        PassthroughFeature.EnableVideoSeeThrough = true;
+        //PassthroughFeature.EnableVideoSeeThrough = true;
+        PXR_Manager.EnableVideoSeeThrough = true;
     }
 
     void OnApplicationPause(bool pause)
     {
         if (pause)
         {
-            PassthroughFeature.EnableVideoSeeThrough = false;
+            //PassthroughFeature.EnableVideoSeeThrough = false;
+            PXR_Manager.EnableVideoSeeThrough = false;
         }
         else
         {
-            PassthroughFeature.EnableVideoSeeThrough = true;
+            //PassthroughFeature.EnableVideoSeeThrough = true;
+            PXR_Manager.EnableVideoSeeThrough = true;
         }
     }
 }
